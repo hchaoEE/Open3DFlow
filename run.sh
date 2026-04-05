0d4ccba08eaf89bdb4bf15c5e2defff6657f1230
hchaoEE <hchao1990@126.com>
2026-04-06 00:25:43 +0800

    fix bug

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 echo "Begin Hardening Process"
 export DESIGN_CONFIG=designs/180_180/bottom_die/config.mk
-#make
-make do-5_3_route
+make
+#make do-5_3_route
 echo "Finish harden the bottom die"
-# sleep 2
-# export DESIGN_CONFIG=designs/180_180/top_die/config.mk
-# make
-# echo "ALL Finished"
+sleep 2
+export DESIGN_CONFIG=designs/180_180/top_die/config.mk
+make
+echo "ALL Finished"
